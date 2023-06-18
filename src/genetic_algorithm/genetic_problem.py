ae21184f5ed562676231553d79d6b2297a262c9e
Wabbrik <g.m_gabriel@yahoo.com>
2023-06-18 17:52:25 +0300

    Vectorizes dropout. Makes use of numpy random number generator instance. Temp: limits numpy threads to 1.

--- src/genetic_algorithm/genetic_problem.py
+++ src/genetic_algorithm/genetic_problem.py
@@ -1,4 +1,4 @@
-from typing import Callable, List
+from typing import Callable
 
 import numpy as np
 from pymoo.problems.functional import FunctionalProblem
@@ -7,13 +7,17 @@ from overlap.metrics import tanimoto
 from overlap.weights import boltzmann_weights
 from spectrum.experimental_spectrum import ExperimentalSpectrum
 
+_rng = np.random.default_rng(seed=123)
 
-def fitness(x_energies: np.ndarray, es: ExperimentalSpectrum, constant: str = "kcal/mol") -> np.array:
+
+def fitness(
+    x_energies: np.ndarray,
+    es: ExperimentalSpectrum,
+    constant: str = "kcal/mol",
+    dropout_percentage: float = 0.08
+) -> np.array:
     weights = boltzmann_weights(x_energies, constant)
-    percentage = 0.08
-    for i in range(len(weights)):
-        if np.random.random() < percentage:
-            weights[i] = 0
+    weights[_rng.random(len(weights)) < dropout_percentage] = 0.0
     spectra_accumulator = es.simulated_vals(weights)
     return tanimoto(spectra_accumulator, es.vals(es.freq_range))
 
