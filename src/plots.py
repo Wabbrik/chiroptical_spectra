4ca39fda6b7666c60952f7dfb78ba5ebf2aabfe8
Wabbrik <g.m_gabriel@yahoo.com>
2023-07-14 18:47:54 +0300

    quick fix broadening

--- src/plots.py
+++ src/plots.py
@@ -6,43 +6,30 @@ import matplotlib.pyplot as plt
 
 from overlap.metrics import tanimoto
 from parameters.input_parameters import InputParameters
-from parameters.utils import get_first_integer, plot_results, write_results
+from parameters.utils import plot_results, write_results
 
 def main() -> int:
     ip = InputParameters(path=join(getcwd(), "GA_Analysis_File.json"))
     
-    candidates = [spectrum for spectrum in ip.experimental_spectra if spectrum.is_opt_candidate]
-    correlations = dict()
-    for candidate in candidates:
-        for key, candidate_broadened in candidate.broadened.items():
-            correlation = (tanimoto(candidate.vals(candidate.freq_range), candidate_broadened.vals(candidate.freq_range)) + 1) / 2
-            correlations[get_first_integer(key)] = correlation
-        correlation_sum = sum(correlations.values())
-        correlation_average = correlation_sum / len(correlations)
-        for key in correlations.keys():
-            correlations[key] = (correlations[key] - correlation_average) # / correlation_sum
+    # candidates = [spectrum for spectrum in ip.experimental_spectra if spectrum.is_opt_candidate]
+    # correlations = defaultdict(list)
+    # for i, candidate in enumerate(candidates):
+    #     for candidate_broadened_vals in candidate.broadened_vals:
+    #         correlations[i].append(tanimoto(candidate.vals(candidate.freq_range), candidate_broadened_vals))
 
     # for correlation in correlations.values():
     #     plt.bar(list(range(len(correlation))), correlation)
     #     plt.show()        
-    def load_file(filename: str):
-        weights_per_conformer = {}
-        with open(filename, "r") as file:
-            for line in file.readlines():
-                k, value = line.split()
-                weights_per_conformer[int(k)] = float(value)
 
-        return weights_per_conformer
-        # return list(dict(sorted(weights_per_conformer.items())).values())
+    weights_per_conformer = {}
+    with open("gaBWs0.786", "r") as file:
+        for line in file.readlines():
+            k, value = line.split()
+            weights_per_conformer[int(k)] = float(value)
+
+    weights = list(dict(sorted(weights_per_conformer.items())).values())
     
-    weights_list = []
-    weights_list.append(load_file("gaBWs0.746"))
-    weights_list.append(load_file("gaBWs0.671"))
-    weights_list.append(correlations)
-
-    for weights in weights_list:
-        plt.bar(list(weights.keys()), list(weights.values()), alpha=0.5)
-
+    plt.bar(list(range(len(weights))), weights)
     plt.show()
 
     return 0
